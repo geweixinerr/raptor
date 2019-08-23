a76554864f2265a2ab8440e57fdbf577313ea129
geweixin <geweixin@d2cmall.com>
2019-08-23 17:23:36 +0800

    rpc server新增手工启动方式

---
+++ raptor-core/src/main/java/raptor/core/server/RpcServerManualBuilder.java
@@ -0,0 +1,85 @@
+package raptor.core.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ApplicationContext;
+
+import raptor.core.annotation.RpcHandler;
+import raptor.core.init.RpcMapping;
+import raptor.core.init.RpcParameter;
+import raptor.core.server.task.RpcServerMonitor;
+
+/**
+ * @author gewx RPC Server 手工配置构建启动类
+ **/
+public final class RpcServerManualBuilder {
+
+	private static final String ADDRESS_KEY = "localAddress";
+
+	private static final String PORT = "port";
+
+	private final Map<String, String> CONFIG_MAP;
+
+	private final ApplicationContext context;
+
+	private RpcServerManualBuilder(String address, String port, ApplicationContext context) {
+		CONFIG_MAP = new HashMap<>();
+		CONFIG_MAP.put(ADDRESS_KEY, address);
+		CONFIG_MAP.put(PORT, port);
+		this.context = context;
+	}
+
+	public void start() throws InterruptedException {
+		checkConfig();
+
+		RpcParameter.INSTANCE.initRpcParameter(CONFIG_MAP);
+		RpcServerTaskPool.initPool();
+		RpcServer.start();
+		RpcServerMonitor.scan();
+
+		if (context != null) {
+			Map<String, Object> rpcMap = context.getBeansWithAnnotation(RpcHandler.class);
+			RpcMapping.initRpcMapping(rpcMap);
+		}
+	};
+
+	public static class Builder {
+
+		private String address;
+
+		private String port;
+
+		private ApplicationContext context;
+
+		public Builder address(String address) {
+			this.address = address;
+			return this;
+		}
+
+		public Builder port(String port) {
+			this.port = port;
+			return this;
+		}
+
+		public Builder setContext(ApplicationContext context) {
+			this.context = context;
+			return this;
+		}
+
+		public RpcServerManualBuilder build() {
+			return new RpcServerManualBuilder(this.address, this.port, this.context);
+		}
+	}
+
+	private void checkConfig() {
+		if (StringUtils.isBlank(CONFIG_MAP.get(ADDRESS_KEY))) {
+			throw new NullPointerException("Rpc Server Configuration --> localAddress is Empty!");
+		}
+
+		if (StringUtils.isBlank(CONFIG_MAP.get(PORT))) {
+			throw new NullPointerException("Rpc Server Configuration --> port is Empty!");
+		}
+	}
+}
